2a29eecf0b31e5fad33566f3494e0d3c3af892a5
RYAN VALDIVIA <rvaldiviase@unsa.edu.pe>
2024-01-17 22:13:18 -0500

    Subiendo el codigo perl y js para que sea funcional el register

---
+++ js/register.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const formRegister = document.getElementById('formRegister');
+
+    formRegister.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(formRegister);
+
+        fetch('cgi/register.pl', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success === 1) {
+                alert('Registro exitoso');
+                // Puedes redirigir o hacer otras acciones después de un registro exitoso
+            } else {
+                alert(data.message);
+            }
+        })
+        .catch(error => console.error('Error:', error));
+    });
+});
