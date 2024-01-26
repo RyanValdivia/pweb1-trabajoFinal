d6e780bb789c964771012a7517ea57dd08634813
JhonatanDczel <jariasq@unsa.edu.pe>
2024-01-26 07:59:14 -0500

    javascript & node >>>>> perl & cgi

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,5 @@
 const prev = document.querySelector(".prev");
 const slide = document.querySelector(".slide");
-
 const next = document.querySelector(".next");
 
 prev.addEventListener("click", () => {
@@ -10,3 +9,30 @@ prev.addEventListener("click", () => {
 next.addEventListener("click", () => {
   slide.scrollLeft += 300;
 });
+
+const obtenerLibros = async function () {
+  try {
+    const response = await fetch("http://localhost:3000/obtener-imagenes");
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error al obtener imágenes: ", error);
+    throw error;
+  }
+};
+
+const cargarLibros = async function () {
+  try {
+    const libros = await obtenerLibros();
+
+    libros.forEach((libro) => {
+      let portada = document.createElement("img");
+      portada.src = "./images_libros/" + libro.rutaDePortada;
+      slide.appendChild(portada);
+    });
+  } catch (error) {
+    console.error("Error al cargar libros: ", error);
+  }
+};
+
+cargarLibros();
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+const port = 3000;
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "biblioteca",
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error al conectar a la base de datos:", err);
+  } else {
+    console.log("Conexión exitosa a la base de datos");
+  }
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/obtener-imagenes", (req, res) => {
+  const query = "SELECT rutaDePortada, descripcion FROM libros";
+
+  connection.query(query, (err, resultados) => {
+    if (err) {
+      console.error("Error al obtener imágenes desde la base de datos:", err);
+      res
+        .status(500)
+        .json({ error: "Error al obtener imágenes desde la base de datos" });
+    } else {
+      res.json(resultados);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Servidor Node.js en ejecución en http://localhost:${port}`);
+});
