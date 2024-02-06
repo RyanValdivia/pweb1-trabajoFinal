0e0cd216a50f16fd6407544473c0d504068d5bf4
JhonatanDczel <jariasq@unsa.edu.pe>
2024-02-05 19:55:57 -0500

    4... intento 4

--- changeIP.sh
+++ changeIP.sh
@@ -3,7 +3,7 @@
 ip_address=$(curl -s ifconfig.me)
 
 for file in "./plantillas-cgi/nav-component.js" "./js/libros.js" "./js/slide.js"; do
-    echo "const ip = \"$ip_address\" || \"localhost\";" > temp
+    echo "var ip = \"$ip_address\" || \"localhost\";" > temp
     
     tail -n +2 "$file" >> temp
     
