f76129a54322b2892ffb12fdff14cc79826dfb45
RyanValdivia <rvaldiviase@unsa.edu.pe>
2024-02-06 12:08:11 -0500

    Tratando de hacer funcional el profile

--- js/profile.js
+++ js/profile.js
@@ -5,6 +5,7 @@ const tag = document.querySelector("#tag");
 fetch(`cgi/profile.pl?id=${sessionStorage.getItem("userId")}`)
   .then((response) => response.json())
   .then((data) => {
+    console.log(data);
     user.innerHTML = data.username;
     email.innerHTML = data.email;
   })
