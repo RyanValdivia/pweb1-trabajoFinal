bae47db9f848deeef7b57f990642fb954a4a6ad8
RyanValdivia <rvaldiviase@unsa.edu.pe>
2024-02-06 12:56:08 -0500

    Haciendo que la contra se muestre

--- js/profile.js
+++ js/profile.js
@@ -4,15 +4,18 @@ fetch(`cgi/profile.pl?id=${sessionStorage.getItem("userId")}`)
     // Manipular los datos recibidos
     const username = data.username;
     const email = data.email;
+    const pass = data.password;
 
     const nombreP = document.getElementById("nombre");
     const tagP = document.getElementById("tag");
     const usernameP = document.getElementById("userName");
     const emailP = document.getElementById("email");
+    const passP = document.getElementById("password");
 
     nombreP.textContent = username;
     tagP.textContent = "@" + username;
     usernameP.textContent = username;
+    passP.textContent = pass;
 
     emailP.textContent = email;
   })
