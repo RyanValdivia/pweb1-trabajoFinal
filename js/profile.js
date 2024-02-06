4d0a933d362030f630ab2563faa12525241f93d1
JhonatanDczel <jariasq@unsa.edu.pe>
2024-02-06 12:26:27 -0500

    intento 4

--- js/profile.js
+++ js/profile.js
@@ -1,21 +1,19 @@
-let userData;
     fetch(`cgi/profile.pl?id=${sessionStorage.getItem('userId')}`)
     .then(response => response.json())
             .then(data => {
                 // Manipular los datos recibidos
-                userData = data;
+                const username = data.username;
+                const email = data.email;
+
+                const nombreP = document.getElementById('nombre');
+                const tagP = document.getElementById('tag');
+                const usernameP = document.getElementById('username');
+                const emailP = document.getElementById('email');
+
+                nombrep.textContent = username;
+                tagP.textContent = username;
+                usernameP.textContent = username;
+
+                emailP.textContent = email;
             })
             .catch(error => console.error('Error:', error));
-const username = userData.username;
-const email = userData.email;
-
-const nombreP = document.getElementById('nombre');
-const tagP = document.getElementById('tag');
-const usernameP = document.getElementById('username');
-const emailP = document.getElementById('email');
-
-nombrep.textContent = username;
-tagP.textContent = username;
-usernameP.textContent = username;
-
-emailP.textContent = email;
